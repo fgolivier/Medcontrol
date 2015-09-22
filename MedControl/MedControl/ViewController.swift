eb574e5903b39c743a41e33450721a76cac2a88d
fgolivier <deoliveira@gmail.com>
2015-09-22 20:10:40 -0300

    Criação do projeto

---
+++ MedControl/MedControl/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MedControl
+//
+//  Created by Admin on 22/09/15.
+//  Copyright (c) 2015 Puc-CCE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
