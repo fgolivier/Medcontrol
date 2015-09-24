5af3140cf62d3e1982dff4757500ea98d0d31682
Marcio Moritz <Marcio@Marcios-MacBook-Pro.local>
2015-09-23 22:12:00 -0300

    Storyboard update
    constraints , new fields

--- MedControl/MedControl/MedControlViewController.swift
+++ MedControl/MedControl/MedControlViewController.swift
@@ -8,16 +8,36 @@
 
 import UIKit
 
-class MedControlViewController: UIViewController {
+class MedControlViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var medCtlTableViewOutlet: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.medCtlTableViewOutlet.delegate = self
+        self.medCtlTableViewOutlet.dataSource = self
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let emUsoMedCtl: MedCtlTableViewCell = tableView.dequeueReusableCellWithIdentifier("controleMedicamentoCell") as! MedCtlTableViewCell
+        
+        return emUsoMedCtl
+    }
+    
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        
     }
 
 
---
+++ MedControl/MedControl/MedCtlTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MedCtlTableViewCell.swift
+//  MedControl
+//
+//  Created by Marcio Moritz on 9/23/15.
+//  Copyright © 2015 Puc-CCE. All rights reserved.
+//
+
+import UIKit
+
+class MedCtlTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
